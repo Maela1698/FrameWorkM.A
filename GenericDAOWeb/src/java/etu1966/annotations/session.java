2cfbe4d3939eeee3681362c75ad42e028a2986f5
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2023-07-07 18:28:47 +0300

    Sprint12OK

---
+++ GenericDAOWeb/src/java/etu1966/annotations/session.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu1966.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ *
+ * @author to
+ */
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface session {
+    
+}
